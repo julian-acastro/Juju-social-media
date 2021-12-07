d7f28bf2c139b3b3fb25f16a81dd6c2c3b946ea6
Judit Meaca <juditmeaca@gmail.com>
2021-12-07 00:26:13 -0300

    Agregue funcionalidad Like

--- js/js.js
+++ js/js.js
@@ -1,6 +1,12 @@
 const loader = document.querySelector(".loader");
 const container = document.querySelector(".container");
 const header = document.querySelector(".header");
+const like = document.getElementById("like");
+const cantLike = document.querySelector("cantLike");
+
+function myFunction(x) {
+    x.classList.toggle("fas");    
+  }
 
 function iniciar(){
     setTimeout(() =>{
@@ -8,7 +14,6 @@ function iniciar(){
         loader.style.display = 'none';
 
         container.style.opacity = 1;
-        header.style.opacity = 1;
     }
     ,Math.random() * (2000 - 500) + 500);
 }
