81e4e5335fe13e1c49f85a74946cd63d7bfa0582
julian castro <abaddon.castro@gmail.com>
2021-12-07 16:33:29 -0300

    me gusta

--- js/js.js
+++ js/js.js
@@ -4,9 +4,11 @@ const header = document.querySelector(".header");
 const like = document.getElementById("like");
 
 
-function myFunction(x) {
-    x.classList.toggle("fas");    
-    var elemento = document.querySelector(".numero");
+
+function myFunction(x , numerito) {
+    x.classList.toggle("fas"); 
+    console.log(numerito);
+    var elemento = document.getElementById(numerito);
     var num = parseInt(elemento.innerHTML);
     if(x.classList.contains("fas")){
         num+=1;
